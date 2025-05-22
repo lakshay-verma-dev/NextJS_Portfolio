9dd9e0e824a0be83cc1d6a1259e3bae5e94bb23c
Lakshay-frontend-Dev <vermaff495@gmail.com>
2025-05-22 15:37:39 +0530

    size

--- src/components/SectionHeader.tsx
+++ src/components/SectionHeader.tsx
@@ -13,7 +13,7 @@ export function SectionHeader({
       whileInView={{ opacity: 1, x: 0 }}
       transition={{ duration: 0.7, ease: "easeOut", delay: 0.1 }}
       viewport={{ once: true }}
-      className={`text-2xl sm:text-4xl md:text-5xl lg:text-6xl font-extrabold text-center leading-tight mb-12 bg-gradient-to-r from-purple-500 to-blue-500 bg-clip-text text-transparent ${className}`}
+      className={`text-4xl sm:text-4xl md:text-5xl lg:text-6xl font-extrabold text-center leading-tight mb-12 bg-gradient-to-r from-purple-500 to-blue-500 bg-clip-text text-transparent ${className}`}
     >
       {children}
     </motion.h2>
